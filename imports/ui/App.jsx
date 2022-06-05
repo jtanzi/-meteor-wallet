a9cea09f68a37d40dc49f09afe76d95c03eccfbf
Joel Tanzi <jtanziredux@gmail.com>
2022-06-05 13:54:06 -0500

    Setup and configure TailwindCSS

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -4,7 +4,7 @@ import { ContactList } from './ContactList';
 
 export const App = () => (
   <div>
-    <h1>Welcome to Meteor!</h1>
+    <h1 className="text-3xl text-indigo-800">Welcome to Meteor!</h1>
     <ContactForm />
     <ContactList />
   </div>
